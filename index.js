104027d7e9f8bc7a64123d4e3016189b4b52adb3
glennfaison <glennfaison@gmail.com>
2018-07-28 16:07:16 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,90 @@
+var JsonValidator = function() {};
+
+JsonValidator.types = {
+  "String": String.prototype.constructor,
+  "Number": Number.prototype.constructor,
+  "Array": Array.prototype.constructor,
+  "Buffer": Buffer.prototype.constructor,
+  "Object": Object.prototype.constructor,
+};
+
+var Attribute = function(){
+  this.valueType;
+  this.defaultValue = null;
+  this.isNullable = true;
+  this.isRequired = true;
+};
+// Set the attribute valueType.
+Attribute.prototype.isString = function() {
+  this.valueType = JsonValidator.types.String;
+  return this;
+};
+Attribute.prototype.isNumber = function() {
+  this.valueType = JsonValidator.types.Number;
+  return this;
+};
+Attribute.prototype.isArray = function() {
+  this.valueType = JsonValidator.types.Array;
+  return this;
+};
+Attribute.prototype.isBuffer = function() {
+  this.valueType = JsonValidator.types.Buffer;
+  return this;
+};
+Attribute.prototype.isObject = function() {
+  this.valueType = JsonValidator.types.Object;
+  return this;
+};
+
+// Set the attribute defaultValue.
+Attribute.prototype.hasDefaultValue = function(defaultValue) {
+  this.defaultValue = defaultValue;
+  return this;
+};
+// Set the attribute isNullable.
+Attribute.prototype.isNotNull = function() {
+  this.isNullable = false;
+  return this;
+};
+// Set the attribute isRequired.
+Attribute.prototype.isNotRequired = function() {
+  this.isRequired = false;
+  return this;
+};
+
+// Create Attribute object in Schema.
+JsonValidator.setAttributes = function() {
+  return new Attribute();
+};
+JsonValidator.createSchema = function(schemaProps, extensible=false) {
+  // Perform error checks first.
+  let schema = {};
+  schema.properties = schemaProps;
+  schema.isExtensible = extensible;
+  return schema;
+};
+JsonValidator.validateJson = function(schema, jsonObject) {
+  for(let prop in schema.properties) {
+    if(!jsonObject.hasOwnProperty(prop) && schema.properties[prop].isRequired) {
+      return false;
+    }
+    if(jsonObject[prop] === null && schema.properties[prop].isNotNull) {
+      return false;
+    }
+    if(jsonObject[prop].constructor !== schema.properties[prop].valueType) {
+      return false;
+    }
+    if(!Object.keys(schema.properties).includes(prop) && !schema.isExtensible) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+module.exports.JsonValidator = JsonValidator;
+module.exports.Attribute = Attribute;
+module.exports.createSchema = JsonValidator.createSchema;
+module.exports.setAttributes = JsonValidator.setAttributes;
+module.exports.validateJson = JsonValidator.validateJson;
+module.exports.types = JsonValidator.types;
---
+++ readme.md
@@ -0,0 +1,19 @@
+# <b>JSON Object Validator</b>
+A tool to define templates for JSON objects, and validate object properties according these templates.
+## <b>Usage</b>
+Below is an example of how to use json-object-validator.
+<code>
+  >
+    // Import json-object-validator.
+    const jv = require('json-object-validator');
+
+    // Create a schema/template for your JSON objects.
+    var schema = jv.createSchema({
+      foo: jv.setAttributes().isString().isNotNull().isNotRequired(),
+      bar: jv.setAttributes().isNumber().isNotNull().hasDefaultValue(0)
+    });
+
+    // Test your objects against the schema.<br/>
+    let jsonObject = { foo: "foo", bar: 2 };<br/>
+    console.log(jv.validateJson(schema, jsonObject)); // Returns true.
+</code>
