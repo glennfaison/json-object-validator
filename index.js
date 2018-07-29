49607eba4460f8153c4e0c4fdc135b136dc3ff3a
glennfaison <glennfaison@gmail.com>
2018-07-29 17:05:22 +0100

    Renamed the validateJson method to objectIsValid, added error-checking to the objectIsValid method.

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
+/**
+ * @description Constructor for {JsonValidator}
+ */
 var JsonValidator = function() {};
 
 JsonValidator.types = {
-  "String": String.prototype.constructor,
-  "Number": Number.prototype.constructor,
-  "Array": Array.prototype.constructor,
-  "Buffer": Buffer.prototype.constructor,
-  "Object": Object.prototype.constructor,
-  "Boolean": Boolean.prototype.constructor,
+  "String": "String",
+  "Number": "Number",
+  "Array": "Array",
+  "Buffer": "Buffer",
+  "Object": "Object",
+  "Boolean": "Boolean",
+  "Function": "Function",
+  "Date": "Date",
 };
-
+/**
+ * @description Constructor for {Attribute}
+ */
 var Attribute = function(){
-  this.valueType;
+  this.valueType = JsonValidator.types.Object;
   this.defaultValue = null;
   this.isNullable = true;
   this.isRequired = true;
@@ -42,6 +49,11 @@ Attribute.prototype.isBoolean = function() {
 };
 
 // Set the attribute defaultValue.
+/**
+ * @description Sets the default value for a conforming object's property.
+ *
+ * @param {String} directory The directory where the db files should be installed.
+ */
 Attribute.prototype.hasDefaultValue = function(defaultValue) {
   this.defaultValue = defaultValue;
   return this;
@@ -62,13 +74,47 @@ JsonValidator.setAttributes = function() {
   return new Attribute();
 };
 JsonValidator.createSchema = function(schemaProps, extensible=false) {
-  // Perform error checks first.
   let schema = {};
   schema.properties = schemaProps;
   schema.isExtensible = extensible;
+  let schemaSchema = {
+    properties: {
+      valueType: {
+        valueType: JsonValidator.types.String,
+        defaultValue: JsonValidator.types.Object,
+        isNullable: false,
+        isRequired: true
+      },
+      defaultValue: {
+        valueType: JsonValidator.types.Object,
+        defaultValue: null,
+        isNullable: true,
+        isRequired: true
+      },
+      isNullable: {
+        valueType: JsonValidator.types.Boolean,
+        defaultValue: true,
+        isNullable: false,
+        isRequired: true
+      },
+      isRequired: {
+        valueType: JsonValidator.types.Boolean,
+        defaultValue: true,
+        isNullable: false,
+        isRequired: true
+      }
+    },
+    isExtensible: false
+  };
+  // Perform error checks.
+  for(let prop in schema.properties) {
+    if(!JsonValidator.objectIsValid(schemaSchema, schema.properties[prop])) {
+      if(!isValid) { throw new Error("Invalid schema object"); }
+    }
+  }
   return schema;
 };
-JsonValidator.validateJson = function(schema, jsonObject) {
+JsonValidator.objectIsValid = function(schema, jsonObject) {
   for(let prop in schema.properties) {
     if(!jsonObject.hasOwnProperty(prop) && schema.properties[prop].isRequired) {
       return false;
@@ -76,8 +122,24 @@ JsonValidator.validateJson = function(schema, jsonObject) {
     if(jsonObject[prop] === null && schema.properties[prop].isNotNull) {
       return false;
     }
-    if(jsonObject[prop].constructor !== schema.properties[prop].valueType) {
+    if(jsonObject[prop] === null && schema.properties[prop].defaultValue !== null) {
       return false;
+    }
+    if(jsonObject[prop] === null) { continue; }
+    let propConstructorName = jsonObject[prop].constructor.name;
+    if(propConstructorName !== schema.properties[prop].valueType) {
+      if(schema.properties[prop].valueType === JsonValidator.types.Date) {
+        if(propConstructorName === JsonValidator.types.Boolean) { return false; }
+        if(propConstructorName === JsonValidator.types.String) {
+          try {
+            Date.parse(jsonObject[prop]);
+          } catch (error) {
+            return false;
+          }
+        }
+      }
+      if(schema.properties[prop].valueType !== JsonValidator.types.Object &&
+        schema.properties[prop].valueType !== JsonValidator.types.Date) { return false; }
     }
     if(!Object.keys(schema.properties).includes(prop) && !schema.isExtensible) {
       return false;
@@ -91,5 +153,5 @@ module.exports.JsonValidator = JsonValidator;
 module.exports.Attribute = Attribute;
 module.exports.createSchema = JsonValidator.createSchema;
 module.exports.setAttributes = JsonValidator.setAttributes;
-module.exports.validateJson = JsonValidator.validateJson;
+module.exports.objectIsValid = JsonValidator.objectIsValid;
 module.exports.types = JsonValidator.types;
